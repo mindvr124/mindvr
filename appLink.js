9cdaf54d930fa744423872c3da840ac2de13bc07
mindvr124 <160832756+mindvr124@users.noreply.github.com>
2024-05-31 18:36:38 +0900

    Update appLink.js

--- appLink.js
+++ appLink.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const page = urlParams.get('room');
+    const page2 = urlParams.get('roomid');
 
     const appStoreURL = 'https://play.google.com/store/apps/details?id=com.Mindvridge'; // 앱 스토어 URL (Google Play 예시)
     const appStoreURLiOS = 'https://apps.apple.com/app/id6449755259'; // 앱 스토어 URL (App Store 예시)
     const userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-    var url = window.location.search;
-    var roomName = url.split("mindvr");
-    const appScheme = "unitydl://mindvridge" + roomName[1];
+    const appScheme = `unitydl://mindvridge?${page}&${page2}`;
 
     if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
         // iOS
@@ -23,8 +24,6 @@ document.addEventListener("DOMContentLoaded", function() {
         const startTime = new Date().getTime();
         const timeout = 1500;
         window.location.href = appScheme;
-
-        /*
         setTimeout(function() {
             const endTime = new Date().getTime();
             if (endTime - startTime < timeout + 100) {
@@ -32,8 +31,5 @@ document.addEventListener("DOMContentLoaded", function() {
             }
             // `window.close();` 제거됨: 대부분의 브라우저에서는 동작하지 않음.
         }, timeout);
-
-
-         */
     }
 });
