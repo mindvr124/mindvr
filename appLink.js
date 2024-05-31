3a00a60ec5fcf8f80fe3a4429486d0a4f55337dc
mindvr124 <160832756+mindvr124@users.noreply.github.com>
2024-05-31 18:48:22 +0900

    Update appLink.js

--- appLink.js
+++ appLink.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const appStoreURLiOS = 'https://apps.apple.com/app/id6449755259'; // 앱 스토어 URL (App Store 예시)
     const userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-    const appScheme = `unitydl://mindvridge?${page}&${page2}`;
+    const appScheme = `unitydl://mindvridge?${encodeURIComponent(page)}&${encodeURIComponent(page2)}`;
 
     if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
         // iOS
